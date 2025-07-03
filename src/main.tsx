6cdfc5f5dc21853ce2c2ba371415d2e3823cb0f4
serendipetey <85279893+serendipetey@users.noreply.github.com>
2025-07-03 13:39:18 +1000

    refactor: update CSS import order and clean up unused tokens

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Try importing CSS via JavaScript
+import "@serendipetey/design-tokens/dist/css/index.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
